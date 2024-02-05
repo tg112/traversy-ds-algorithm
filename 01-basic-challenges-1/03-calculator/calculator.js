56896a573cc5ceb6f5aca3d0dd81d3073b1b58ed
tg112 <info.tgw112@gmail.com>
2024-02-05 22:36:35 +0900

    finish calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === '+') {
+    return num1 + num2;
+  }
+  if (operator === '-') {
+    return num1 - num2;
+  }
+  if (operator === '/') {
+    return num1 / num2
+  }
+  if (operator === '*') {
+    return num1 * num2;
+  }
+  throw new Error('got unexpected operator')
+}
 
 module.exports = calculator;
