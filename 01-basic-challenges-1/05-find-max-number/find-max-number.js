f0054b17f1b1f5a9b6ac7940b6331e475054e4a5
tg112 <info.tgw112@gmail.com>
2024-02-05 23:28:05 +0900

    findMaxNumber and count occureneces

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(nums) {
+  // return Math.max(...nums);
+  let maxNumber = nums[0];
+  for (let num of nums) {
+    if (maxNumber < num) {
+      maxNumber = num;
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
