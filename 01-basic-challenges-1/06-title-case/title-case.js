66f714ec7d39a0f97eaafba656dd763724f8ff2f
tg112 <info.tgw112@gmail.com>
2024-02-06 20:50:25 +0900

    finish title-case and reverse-string

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  const splitedWords = str.split(' ');
+  let sentence = [];
+  for (let word of splitedWords) {
+    sentence.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+  }
+  return sentence.join(' ');
+}
 
 module.exports = titleCase;
