94e4e4a09eb9d7c3e0ee47329c5cf589b670676a
tg112 <info.tgw112@gmail.com>
2024-02-07 21:59:28 +0900

    finish display-likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,20 @@
-function displayLikes() {}
+function displayLikes(people) {
+  if (people.length === 0) {
+    return 'no one likes this';
+  }
+  if (people.length === 1) {
+    return `${people[0]} likes this`;
+  }
+  if (people.length === 2) {
+    return `${people[0]} and ${people[1]} like this`;
+  }
+  if (people.length === 3) {
+    return `${people[0]}, ${people[1]} and ${people[2]} like this`;
+  }
+  if (people.length > 3) {
+    return `${people[0]}, ${people[1]} and ${people.length - 2} others like this`;
+  }
+
+}
 
 module.exports = displayLikes;
