34ebc84bf8bb08f266ce0fab5d05644b7414aa30
tg112 <info.tgw112@gmail.com>
2024-02-07 21:55:01 +0900

    finish fizzbuzz and array-intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const arr = [];
+  for (let num of arr1) {
+    if (arr2.includes(num) && !arr.includes(num)) {
+      arr.push(num);
+    }
+  }
+  return arr;
+}
 
 module.exports = arrayIntersection;
