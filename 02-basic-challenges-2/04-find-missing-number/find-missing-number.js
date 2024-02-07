e0041853980506db40be12abaa71935a41d20481
tg112 <info.tgw112@gmail.com>
2024-02-07 23:32:29 +0900

    finish find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,9 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  for (let i = 1; i <= arr.length; i++) {
+    if (!arr.includes(i)) {
+      return i;
+    }
+  }
+}
 
 module.exports = findMissingNumber;
