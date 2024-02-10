068120c9fae746c164a84c5f8e1066744458aca7
tg112 <info.tgw112@gmail.com>
2024-02-10 22:43:13 +0900

    finish are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,14 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(word) {
+  const charMap = {};
+
+  for (let char of word) {
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
